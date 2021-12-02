5f41ce9122f52fb5975233a79fa644c61969de8b
Turbo <78885232+Turbonator0@users.noreply.github.com>
2021-12-02 18:19:59 +0200

    It looks bad :(

--- dynamic.js
+++ dynamic.js
@@ -1,27 +1,6 @@
-// too lazy to type this whole thing :)
-function RemByID(id){
-    document.getElementById(id).remove();
-}
-function DisplayMenu(){
-    const menu = document.getElementById("p-config-menu");
-    menu.style.visibility = "visible";
-}
+// Speed control for the gradient, very good
 function GradientSpeedControl(){
     var GradientSpeed = document.getElementById("GradientSpeed").value;
-    console.log(GradientSpeed)
-    document.getElementById("top-gradient-thing").style.animationDuration = String(GradientSpeed+"s");
-}
-// stolen from StackOverflow, I don't know anything about JS
-function CheckCommand(){
-    var userInput = document.getElementById("TTY-submit");
-    if((userInput.value=="onetapv2.dll")){
-        document.getElementById("Access").innerHTML= "Successfully injected to running process index.html";
-        RemByID("TTY-submit");
-        RemByID("SubmitCommand");
-        DisplayMenu();
-    }
-    else{
-        document.getElementById("Access").innerHTML="DLL couldn't be found...";
-    }
-
+    console.log(GradientSpeed);
+    document.documentElement.style.setProperty("--global-grad-speed",GradientSpeed+"s")
 }
