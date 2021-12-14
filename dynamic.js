34ae625371a2ceaef07c819d2cc03853df3603a7
Turbo <78885232+Turbonator0@users.noreply.github.com>
2021-12-14 12:18:31 +0200

    Cleaned up stuff

--- dynamic.js
+++
@@ -1,6 +0,0 @@
-// Speed control for the gradient, very good
-function GradientSpeedControl(){
-    var GradientSpeed = document.getElementById("GradientSpeed").value;
-    console.log(GradientSpeed);
-    document.documentElement.style.setProperty("--global-grad-speed",GradientSpeed+"s")
-}
